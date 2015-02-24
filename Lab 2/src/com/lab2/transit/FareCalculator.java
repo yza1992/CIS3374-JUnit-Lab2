c721f03fbd3014e2305aeefc3641bfb48db972a7
Kevin Rothenberger <kevin.rothenberger@gmail.com>
2015-02-23 21:53:56 -0500

    Added Javadoc for calculateFare() method

--- Lab 2/src/com/lab2/transit/FareCalculator.java
+++ Lab 2/src/com/lab2/transit/FareCalculator.java
@@ -2,6 +2,13 @@ package com.lab2.transit;
 
 public class FareCalculator {
 	
+	/**
+	 * 
+	 * @param age - The age of the person
+	 * @param time - The time in military (24-hour) format e.g. 4:00 PM would be 16:00 
+	 * @param isHoliday - If the day is a holiday or not
+	 * @return - 0.0 if discounted fare, 2.5 if regular fare
+	 */
 	public static double calculateFare(int age, String time, boolean isHoliday) {
 		
 		String[] parsedTime = time.split(":");
@@ -9,7 +16,7 @@ public class FareCalculator {
 		int hour = Integer.parseInt(parsedTime[0]);
 		int minutes = Integer.parseInt(parsedTime[1]);
 		
-		if(age <= 5 || age >= 65) {
+		if(age <= 5 || age >= 65) {	
 			if(!isHoliday) {
 				if(hour < 7 || (hour == 9 && minutes > 0) || hour > 9) {
 					return 0.0;
